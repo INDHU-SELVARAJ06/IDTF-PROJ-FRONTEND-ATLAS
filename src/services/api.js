6a5753ebdb583562e6a1c8ec3e80f7b0236f43e0
INDHU-SELVARAJ06 <indhuselvaraj06@gmail.com>
2026-04-05 23:30:23 +0530

    Updated API URL for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://idtf-proj-backend-atlas.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('idtp_token');
